e8b8adc9534aa7d19af729ee7da350db4e354005
Derek Howard <derekhoward@newton.local>
2019-02-26 14:42:09 -0800

    Filtering complete

--- conduit/apps/articles/urls.py
+++ conduit/apps/articles/urls.py
@@ -12,18 +12,24 @@ router.register(r'articles', ArticleViewSet)
 
 app_name = 'articles'
 urlpatterns = [
-    path('', include(router.urls)),
     path('articles/feed', ArticlesFeedAPIView.as_view()),
 
-    path('articles/<slug:article_slug>/favorite',
-        ArticlesFavoriteAPIView.as_view()),
+    path(
+        'articles/<slug:article_slug>/favorite',
+        ArticlesFavoriteAPIView.as_view()
+        ),
 
-    path('articles/<slug:article_slug>/comments',
-        CommentsListCreateAPIView.as_view()),
+    path(
+        'articles/<slug:article_slug>/comments',
+        CommentsListCreateAPIView.as_view()
+        ),
 
-    path('articles/<slug:article_slug>/comments/<int:comment_pk>',
-        CommentsDestroyAPIView.as_view()),
+    path(
+        'articles/<slug:article_slug>/comments/<int:comment_pk>',
+        CommentsDestroyAPIView.as_view()
+        ),
 
     path('tags', TagListAPIView.as_view()),
-]
 
+    path('', include(router.urls)),
+]
