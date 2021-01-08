fbb4da5e2f9c35c583e5e4f02ded19c52fd2ab9f
Thiên Toán <toanalien@gmail.com>
2021-01-08 15:20:08 +0700

    Update models.py
    Fix error: AttributeError: 'str' object has no attribute 'decode'

--- conduit/apps/authentication/models.py
+++ conduit/apps/authentication/models.py
@@ -135,4 +135,4 @@ class User(AbstractBaseUser, PermissionsMixin):
             'exp': int(dt.timestamp())
         }, settings.SECRET_KEY, algorithm='HS256')
 
-        return token.decode('utf-8')
+        return token
